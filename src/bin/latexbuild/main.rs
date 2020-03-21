1ecf0b640d0b64dc132d4ce5c2d286dbfa73ad98
Zehua-Chen <peterchen06242000@outlook.com>
2020-03-20 21:04:34 -0500

    [chore] Prepare for Publish

--- src/bin/latexbuild/main.rs
+++ src/bin/latexbuild/main.rs
@@ -3,6 +3,8 @@ use latexbuild::*;
 use std::fs::{create_dir, write};
 use std::path::PathBuf;
 
+mod subcommands;
+
 const ENTRY_STR: &'static str = "\\documentclass{article}
 
 \\begin{document}
@@ -43,51 +45,13 @@ fn main() {
 
     match matches.subcommand() {
         ("clean", Some(clean_m)) => {
-            let mut logger = StdErrLogger::new();
-            let mut latexbuild = LatexBuild {
-                config_path: PathBuf::from(clean_m.value_of("config").unwrap()),
-                logger: &mut logger,
-            };
-
-            latexbuild.clean();
+            subcommands::clean(clean_m);
         }
         ("new", Some(new_m)) => {
-            let name = new_m.value_of("name").unwrap();
-            let project_root = PathBuf::from(name);
-            let mut logger = StdErrLogger::new();
-
-            // Create project dir
-            logger.message("creating project directory");
-            create_dir(&project_root).unwrap();
-
-            // Create config file
-            logger.message("creating config file");
-            let mut config_path = project_root.clone();
-            config_path.push("latexproject");
-            config_path.set_extension("json");
-
-            let config_str = json::stringify_pretty(Project::new(), 2);
-
-            write(&config_path, config_str.as_bytes()).unwrap();
-
-            // Create entry file
-            logger.message("creating entry file");
-            let mut entry_path = project_root.clone();
-            entry_path.push("index");
-            entry_path.set_extension("tex");
-
-            write(&entry_path, ENTRY_STR.as_bytes()).unwrap();
-
-            logger.message("done");
+            subcommands::new(new_m);
         }
         _ => {
-            let mut logger = StdErrLogger::new();
-            let mut latexbuild = LatexBuild {
-                config_path: PathBuf::from(matches.value_of("config").unwrap()),
-                logger: &mut logger,
-            };
-
-            latexbuild.build();
+            subcommands::build(&matches);
         }
     };
 }
